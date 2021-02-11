0641324b74306b2bd4173d6bced240fde5e897d3
ibrahim <ibrahim_shaban_1996@hotmail.com>
2021-02-11 19:00:39 +0200

    Products-Trade market & Manufacturing

--- resources/views/admin/products/tabs/product_size_weight.blade.php
+++ resources/views/admin/products/tabs/product_size_weight.blade.php
@@ -36,12 +36,12 @@
             {!! Form::select('color_id',App\Color::pluck('name_'.lang(),'id'),$product->color_id,['class'=>'form-control','placeholder'=>trans('admin.color_id')]) !!}
         </div>
         <div class="form-group col-md-4 col-lg-4 col-sm-4 col-xs-12">
-            {{--{!! Form::label('trade_id',trans('admin.trade_id')) !!}
-            {!! Form::select('trade_id',App\TradeMark::pluck('name_'.lang(),'id'),$product->trade_id,['class'=>'form-control','placeholder'=>trans('admin.trade_id')]) !!}--}}
+            {!! Form::label('trade_id',trans('admin.trade_id')) !!}
+            {!! Form::select('trade_id',App\TradeMark::pluck('name_'.lang(),'id'),$product->trade_id,['class'=>'form-control','placeholder'=>trans('admin.trade_id')]) !!}
         </div>
         <div class="form-group col-md-4 col-lg-4 col-sm-4 col-xs-12">
-            {{--{!! Form::label('manu_id',trans('admin.manu_id')) !!}
-            {!! Form::select('manu_id',App\Manufacturers::pluck('name_'.lang(),'id'),$product->manu_id,['class'=>'form-control','placeholder'=>trans('admin.manu_id')]) !!}--}}
+            {!! Form::label('manu_id',trans('admin.manu_id')) !!}
+            {!! Form::select('manu_id',App\Manufacturers::pluck('name_'.lang(),'id'),$product->manu_id,['class'=>'form-control','placeholder'=>trans('admin.manu_id')]) !!}
         </div>
         <div class="col-md-12 col-lg-12 col-sm-12">
             {{--{!! Form::label('malls',trans('admin.malls')) !!}--}}
